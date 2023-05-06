562d2b5098e8e4e26148a420c29db580ca2b2a4c
PAJK87 <pajk87@gmail.com>
2023-05-06 17:22:29 -0400

    Adding Welcome page and working on News page

--- FinMinds/app/src/main/java/com/example/finminds/MainActivity.kt
+++ FinMinds/app/src/main/java/com/example/finminds/MainActivity.kt
@@ -6,32 +6,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
 import com.example.finminds.ui.theme.FinMindsTheme
 
 
@@ -45,9 +52,9 @@ class MainActivity : ComponentActivity() {
                ) {
                    Column(
                        horizontalAlignment = Alignment.CenterHorizontally
+
                    ) {
-                       MyAppBar()
-                       ButtonBox()
+                       MyApp()
                    }
                }
             }
@@ -69,34 +76,85 @@ fun MyAppBar(){
                 color = Color.White,
             )
         },
-
-        navigationIcon = {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    imageVector = Icons.Filled.Menu,
-                    contentDescription = "Localized description",
-                    tint = Color.White
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    imageVector = Icons.Filled.Favorite,
-                    contentDescription = "Localized description",
-                    tint = Color.White
-                )
-            }
-        }
     )
-
 }
+
+@Composable
+private fun MyApp(modifier: Modifier = Modifier) {
+
+    var shouldShowHomeScreen by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowHomeScreen) {
+            WelcomeScreen(onContinueClicked = { shouldShowHomeScreen = false})
+        } else {
+            HomeScreen()
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(){
+    Surface(
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            MyAppBar()
+            ButtonBox()
+        }
+    }
+}
+@Composable
+fun WelcomeScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(color = colorResource(id = R.color.Main)) {
+        
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.finminds_logo),
+            contentDescription = "FinMinds Logo",
+        )
+        Text(
+            text = "Welcome to FinMinds!",
+            color = Color.White,
+            fontSize = 30.sp
+        )
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked,
+            colors = ButtonDefaults.buttonColors(Color.White)
+        ){
+            Text(
+                text= "Continue",
+                color = colorResource(id = R.color.Main),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+    }
+}
+
+
+
 
 
 @Composable
 fun ButtonBox(){
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
     ) {
         TopicButton()
         QuizButton()
@@ -114,11 +172,14 @@ fun TopicButton(){
             context.startActivity(intent)
         },
         modifier = Modifier
-            .height(40.dp)
-            .width(140.dp)
+//            .height(40.dp)
+            .width(250.dp)
             .padding(3.dp)
     ) {
-        Text(text = stringResource(R.string.topics))
+        Text(
+            text = stringResource(R.string.topics),
+            fontSize = 30.sp
+        )
     }
 }
 @Composable
@@ -126,25 +187,33 @@ fun QuizButton(){
     Button(
         onClick = { /*TODO*/ },
         modifier = Modifier
-            .height(40.dp)
-            .width(140.dp)
+//            .height(40.dp)
+            .width(250.dp)
             .padding(3.dp)
     ) {
-        Text(text = stringResource(R.string.random_quiz))
+        Text(
+            text = stringResource(R.string.random_quiz),
+            fontSize = 30.sp
+        )
     }
 }
 
 @Composable
 fun NewsButton(){
+    val context = LocalContext.current
     Button(
-        onClick = { /*TODO*/ },
+        onClick = {
+            val intent = Intent(context, NewsActivity::class.java)
+            context.startActivity(intent)
+        },
         modifier = Modifier
-            .height(40.dp)
-            .width(140.dp)
+//            .height(40.dp)
+            .width(250.dp)
             .padding(3.dp)
     ) {
         Text(
-            text = stringResource(R.string.news)
+            text = stringResource(R.string.news),
+            fontSize = 30.sp
         )
     }
 }
@@ -158,6 +227,22 @@ fun Preview() {
         ) {
             MyAppBar()
             ButtonBox()
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview2() {
+    FinMindsTheme {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Surface(color = colorResource(id = R.color.Main)) {
+                WelcomeScreen(onContinueClicked = { /*TODO*/ })
+            }
+
         }
 
     }
--- FinMinds/app/src/main/java/com/example/finminds/NewsActivity.kt
+++ FinMinds/app/src/main/java/com/example/finminds/NewsActivity.kt
@@ -1,43 +1,62 @@
 package com.example.finminds
 
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-
 import com.example.finminds.ui.theme.FinMindsTheme
 
 class NewsActivity : ComponentActivity() {
 
-    val newsView by viewModels<NewsView>()
+    private val newsView by viewModels<NewsView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +66,12 @@ class NewsActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
 
                 ) {
-                    wsjNewsList(articleList = newsView.wsjNewsResponse)
+                    NewsAppBar()
+                    var bloombergList = newsView.bloombergNewsResponse
+                    var wsjList = newsView.wsjNewsResponse
+                    newsList(articleList = wsjList)
                     newsView.getWsjList()
+                    newsView.getBloombergList()
                 }
             }
         }
@@ -66,39 +89,120 @@ fun NewsScreenPreview() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val listOfArticles = arrayOf(NewsData.Articles(
-                source = NewsData.Source(id = "78", name = "jk"),
-                author = "k",
-                title = "k",
-                description = "hj",
+                source = NewsData.Source(id = "78", name = "The Wall Street Journal"),
+                author = "wsj",
+                title = "Federal Reserve Raises Rates, Signals Potential Pause...",
+                description = "Central bank officials remove earlier guidance that had pointed to further increases",
                 url = "hj",
-                urlToImage = "jk",
+                urlToImage = "https://images.wsj.net/im-775148/social",
                 publishedAt = "4/32",
                 content = "Hjshfljkhasldfjhlaksdjfla"
             ))
-            wsjNewsList(articleList = listOfArticles )
+            NewsAppBar()
+            newsList(articleList = listOfArticles )
         }
 
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun wsjNewsList(articleList: Array<NewsData.Articles>) {
+fun NewsAppBar(){
+    val context = LocalContext.current
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color(0xFF4953BB)),
+        title = {
+            Text(
+                "News",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White,
+            )
+        },
+
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Localized description",
+                    tint = Color.White
+                )
+            }
+        },
+        actions = {
+            SourcesDropDownMenu()
+        }
+    )
+
+}
+
+@Composable
+fun SourcesDropDownMenu() {
+    val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier
+//            .wrapContentSize(Alignment.TopEnd)
+    ) {
+        Text(
+            text = "Sources",
+            color = Color.White,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center,
+        )
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "More",
+                tint = Color.White,
+                modifier = Modifier.offset(x = (-10).dp)
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Wall Street Journal") },
+                onClick = { Toast.makeText(context, "Load", Toast.LENGTH_SHORT).show() }
+            )
+            DropdownMenuItem(
+                text = { Text("Bloomberg News") },
+                onClick = { }
+            )
+        }
+    }
+}
+
+
+
+
+@Composable
+fun newsList(articleList: Array<NewsData.Articles>) {
     LazyColumn {
         itemsIndexed(items = articleList) { index, item ->
-            wsjNewsItem(article = item)
+            newsItem(article = item)
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun wsjNewsItem(article: NewsData.Articles) {
+fun newsItem(article: NewsData.Articles) {
     Card(
         modifier = Modifier
             .padding(8.dp, 4.dp)
             .fillMaxWidth()
             .height(110.dp),
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
 
     ) {
         Surface() {
@@ -127,21 +231,21 @@ fun wsjNewsItem(article: NewsData.Articles) {
                 ) {
                     Text(
                         text = article.title,
-                        style = MaterialTheme.typography.headlineMedium,
+                        style = MaterialTheme.typography.titleMedium,
                         fontWeight = FontWeight.Bold
                     )
-                    Text(
-                        text = article.author,
-                        style = MaterialTheme.typography.titleSmall,
-                        modifier = Modifier
-                            .background(
-                                Color.LightGray
-                            )
-                            .padding(4.dp)
-                    )
+//                    Text(
+//                        text = article.source.name,
+//                        style = MaterialTheme.typography.bodySmall,
+//                        modifier = Modifier
+//                            .background(
+//                                Color.LightGray
+//                            )
+//                            .padding(4.dp)
+//                    )
                     Text(
                         text = article.description,
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.bodySmall,
                         maxLines = 2,
                         overflow = TextOverflow.Ellipsis
                     )
@@ -150,5 +254,8 @@ fun wsjNewsItem(article: NewsData.Articles) {
             }
         }
     }
+}
+
+fun generateNewsList(source : String){
 
 }
